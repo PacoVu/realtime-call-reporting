b08a020009d08545783452ee806238817220bf4a
Paco Vu <paco_vu@yahoo.com>
2025-08-06 11:25:44 -0700

    Update new libs

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-var async = require("async");
+// var async = require("async");
 require('dotenv').load()
 
 const { Pool, Client } = require('pg')
@@ -7,7 +7,7 @@ const pool = new Pool({
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  port: process.env.PGPORT
 })
 
 module.exports = {
